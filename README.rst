d41822a3db9f4c2a6a60e6a576c55ed362af7095
Jack Rosenthal <jack@rosenth.al>
2017-12-20 08:39:09 -0700

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,15 @@
+``harmful.js``
+==============
+
+This is a userscript that helps disable harmful web development behaviors.
+Right now, this script:
+
+1. Disables programmatic selection of text (under most cases, I haven't figured
+   out some corner cases).
+2. Disables programmatic access to the clipboard.
+
+In the future, this script intends to do more, such as replacing common
+non-native controls with the standard HTML ones.
+
+It has been tested with the Guerilla Scripting extension for Pale Moon.
+
---
+++ harmful.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// wname                harmfuljs
+// @namespace           harmfuljs
+// @description         Disable harmful JavaScripts behaviors
+// @include             *
+// @grant               unsafeWindow
+// @version             0
+// @author              Jack Rosenthal
+// ==/UserScript==
+
+/* Selections are sacred, not scriptable
+ * (only to be created, modified, or used by the user themselves) */
+
+noselect = function () {
+    throw "Selections are sacred, and cannot be manipulated or accessed by anyone but the user."
+}
+
+unsafeWindow.getSelection = noselect;
+unsafeWindow.document.getSelection = noselect;
+unsafeWindow.document.createRange = noselect;
+unsafeWindow.document.body.createTextRange = noselect;
+unsafeWindow.HTMLInputElement.prototype.select = noselect;
+unsafeWindow.HTMLInputElement.prototype.setSelectionRange = noselect;
+unsafeWindow.HTMLInputElement.prototype.setRangeText = noselect;
+unsafeWindow.HTMLTextAreaElement.prototype.select = noselect;
+unsafeWindow.HTMLTextAreaElement.prototype.setSelectionRange = noselect;
+unsafeWindow.HTMLTextAreaElement.prototype.setRangeText = noselect;
+unsafeWindow.Range.prototype.setStart = noselect;
+unsafeWindow.Range.prototype.setStartBefore = noselect;
+unsafeWindow.Range.prototype.setStartAfter = noselect;
+unsafeWindow.Range.prototype.setEnd = noselect;
+unsafeWindow.Range.prototype.setEndBefore = noselect;
+unsafeWindow.Range.prototype.setEndAfter = noselect;
+unsafeWindow.Range.prototype.select = noselect;
+unsafeWindow.Range.prototype.selectNode = noselect;
+unsafeWindow.Range.prototype.selectNodeContents = noselect;
+unsafeWindow.Selection.prototype.removeAllRanges = noselect;
+unsafeWindow.Selection.prototype.addRange = noselect;
+unsafeWindow.Selection.prototype.removeRange = noselect;
+unsafeWindow.Selection.prototype.empty = noselect;
+unsafeWindow.Selection.prototype.collapse = noselect;
+unsafeWindow.Selection.prototype.setPosition = noselect;
+unsafeWindow.Selection.prototype.collapseToStart = noselect;
+unsafeWindow.Selection.prototype.collapseToEnd = noselect;
+unsafeWindow.Selection.prototype.extend = noselect;
+unsafeWindow.Selection.prototype.setBaseAndExtent = noselect;
+unsafeWindow.Selection.prototype.containsNode = noselect;
+unsafeWindow.Selection.prototype.selectAllChildren = noselect;
+unsafeWindow.Selection.prototype.modify = noselect;
+
+/* The clipboard is sacred, and only for the user to modify */
+origQCS = unsafeWindow.document.queryCommandSupported;
+unsafeWindow.document.queryCommandSupported = function (command) {
+    if (command == "copy") {
+        return false;
+    }
+    return origQCS(command);
+}
+
+origExec = unsafeWindow.document.execCommand;
+unsafeWindow.document.execCommand = function (command) {
+    if (command == "copy") {
+        throw "My clipboard is sacred, and only to be modified by me, the user.";
+    }
+    return origExec(command);
+}
+
+unsafeWindow.clipboardData = undefined;
